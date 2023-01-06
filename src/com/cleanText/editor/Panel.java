22a2b17074a8f7b030ee1fe2fbac49d719340f63
KevinRivero <kevin_xavier_rivero@hotmail.com>
2023-01-06 12:59:21 -0300

    Se agrega la barra de menu sin opciones ni funcionalidades

--- src/com/cleanText/editor/Panel.java
+++ src/com/cleanText/editor/Panel.java
@@ -11,16 +11,36 @@ public class Panel extends JPanel {
   private ArrayList<JTextPane> listaTexto;
   private ArrayList<JScrollPane> listaScroll;
   private int contadorPanel = 0; // se crea un contador para sumar cuantas "ventanas" vamos creando
+  //------------------- Variables para el menu -------------------
+  private JMenuBar menu;
+  private JMenu archivo, editar, seleccion, ver, aparariencia;
+  private JMenuItem item;
 
   public Panel (){
-
+    // se incicializan las variables para el area de texto
     tPane = new JTabbedPane();
     listaTexto = new ArrayList<JTextPane>();
     listaScroll = new ArrayList<JScrollPane>();
 
-    add(tPane);
-    crearPanel();
+    // se inicializan las variables para el menu
+    menu = new JMenuBar();
+    archivo = new JMenu("Archivo");
+    editar = new JMenu("Editar");
+    seleccion = new JMenu("Seleccion");
+    ver = new JMenu("Ver");
+    aparariencia = new JMenu("Apariencia");
 
+    menu.add(archivo);
+    menu.add(editar);
+    menu.add(seleccion);
+    ver.add(aparariencia);
+    menu.add(ver);
+
+
+
+    add(menu);
+    add(tPane); // se agrega al panel principal, que seria el que se crea con el constructor, el contenedor que tiene las areas de texto y se ejecuta la funcion crearPanel
+    crearPanel();
   }
 
   public void crearPanel (){
